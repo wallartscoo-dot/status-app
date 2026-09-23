4d50ece24707d313f03ddb6e2b4e6d91101e555a
wallartscoo-dot <wallarts.coo@gmail.com>
2026-09-24 01:58:31 +0500

    Delete backend directory

--- backend/src/controllers/creator.controller.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from "express";
-import * as creatorService from "../services/creator.service";
-import { asyncHandler } from "../utils/asyncHandler";
-
-export const getByUsername = asyncHandler(async (req: Request, res: Response) => {
-  const { username } = req.params as { username: string };
-  const creator = await creatorService.getCreatorProfile(username, req.user?.sub);
-  res.json({ creator });
-});
--- backend/src/routes/categories.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as categoriesController from "../controllers/categories.controller";
-
-const router = Router();
-
-router.get("/", categoriesController.listCategories);
-
-export default router;
